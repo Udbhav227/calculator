890c0044643e9f4e889a74db3ae7707e662f5ef6
Udbhav <Udbhav227@gmail.com>
2024-07-20 19:25:05 +0530

    add basic functionality

--- script.js
+++ script.js
@@ -1,14 +1,39 @@
 const calculator = document.querySelector('.calculator');
 const keys = document.querySelector('.calculator__keys');
-
+const display = document.querySelector('.calculator__output');
+let array = [];
+let operatorIsPressed = false;
 
 keys.addEventListener('click', e => {
     if (e.target.matches('button')) {
         const key = e.target;
         const action = key.dataset.action;
+        const keyContent = key.textContent;
+        let displayedNum = display.textContent;
 
         if (!action) {
-            console.log('number key')
+            if (displayedNum === '0' || operatorIsPressed) {
+                display.textContent = keyContent;
+                operatorIsPressed = false;
+              } else {
+                display.textContent = displayedNum + keyContent;
+              }
+        }
+
+        if (action === 'decimal') {
+            display.textContent = displayedNum + '.';
+        }
+        
+        if (action === 'clear') {
+            display.textContent = '0';
+            array = [];
+            operatorIsPressed = false;
+        }
+        
+        if (action === 'backspace') {
+            if (displayedNum !== '0') {
+                display.textContent = displayedNum.slice(0, -1) || '0';
+            }
         }
 
         if (
@@ -17,26 +42,24 @@ keys.addEventListener('click', e => {
             action === 'multiply' ||
             action === 'divide'
         ) {
-            console.log('operator key!')
-        }
-
-        if (action === 'decimal') {
-            console.log('decimal key!')
-        }
-
-        if (action === 'clear') {
-            console.log('clear key!')
+            array[0] = parseFloat(displayedNum);
+            array[1] = action;
+            operatorIsPressed = true;
         }
 
         if (action === 'calculate') {
-            console.log('equal key!')
+            calculate();
         }
 
-        if (action === 'backspace') {
-            console.log('backspace key!')
+        function calculate() {
+            array[2] = parseFloat(displayedNum);
+            display.textContent = operate(array[0], array[1], array[2]);
+            console.log(array);
+            array = [display.textContent];
+            operatorIsPressed = false;
         }
     }
-})
+});
 
 function add(a, b) {
     return a + b;
@@ -55,11 +78,11 @@ function divide(a, b) {
     else return "ERROR";
 }
 
-function operate(a, b, op) {
+function operate(a, op, b) {
     switch (op) {
-        case "+": return add(a, b);
-        case "-": return subtract(a, b);
-        case "x": return multiply(a, b);
-        case "/": return divide(a, b);
+        case "add": return add(a, b);
+        case "subtract": return subtract(a, b);
+        case "multiply": return multiply(a, b);
+        case "divide": return divide(a, b);
     }
 }
