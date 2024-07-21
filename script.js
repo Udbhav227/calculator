2650a0112c3f138d0d926091bde6984e0640a951
Udbhav <Udbhav227@gmail.com>
2024-07-21 13:14:42 +0530

    Implement display, operator keys, decimal key and calculate key

--- script.js
+++ script.js
@@ -1,35 +1,42 @@
 const calculator = document.querySelector('.calculator');
 const keys = document.querySelector('.calculator__keys');
 const display = document.querySelector('.calculator__output');
-let array = [];
-let operatorIsPressed = false;
 
 keys.addEventListener('click', e => {
     if (e.target.matches('button')) {
         const key = e.target;
         const action = key.dataset.action;
         const keyContent = key.textContent;
-        let displayedNum = display.textContent;
+        const displayedNum = display.textContent;
+        const previousKeyType = calculator.dataset.previousKeyType;
+
+        Array.from(key.parentNode.children)
+            .forEach(k => k.classList.remove('is-depressed'));
 
         if (!action) {
-            if (displayedNum === '0' || operatorIsPressed) {
+            if (displayedNum === '0' || previousKeyType === 'operator') {
                 display.textContent = keyContent;
-                operatorIsPressed = false;
-              } else {
+            } else {
                 display.textContent = displayedNum + keyContent;
-              }
+            }
+            calculator.dataset.previousKeyType = 'number';
         }
 
         if (action === 'decimal') {
-            display.textContent = displayedNum + '.';
+            if (previousKeyType === 'operator' || previousKeyType === 'calculate') {
+                display.textContent = '0.';
+            } else if (!displayedNum.includes('.')) {
+                display.textContent = displayedNum + '.';
+            }
+            calculator.dataset.previousKeyType = 'decimal';
         }
         
+
         if (action === 'clear') {
             display.textContent = '0';
-            array = [];
-            operatorIsPressed = false;
+            calculator.dataset.previousKeyType = 'clear';
         }
-        
+
         if (action === 'backspace') {
             if (displayedNum !== '0') {
                 display.textContent = displayedNum.slice(0, -1) || '0';
@@ -42,21 +49,43 @@ keys.addEventListener('click', e => {
             action === 'multiply' ||
             action === 'divide'
         ) {
-            array[0] = parseFloat(displayedNum);
-            array[1] = action;
-            operatorIsPressed = true;
+            const firstValue = calculator.dataset.firstValue;
+            const operator = calculator.dataset.operator;
+            const secondValue = displayedNum;
+
+            if (firstValue && 
+                operator && 
+                previousKeyType !== 'operator' &&
+                previousKeyType !== 'calculate'
+            ) {
+                const calcValue = operate(firstValue, operator, secondValue)
+                display.textContent = calcValue;
+                calculator.dataset.firstValue = calcValue;
+            } else {
+                calculator.dataset.firstValue = displayedNum; 
+            }
+
+            key.classList.add('.is-pressed');
+            calculator.dataset.previousKeyType = 'operator';
+            calculator.dataset.firstValue = displayedNum;
+            calculator.dataset.operator = action;
         }
 
         if (action === 'calculate') {
-            calculate();
-        }
+            const firstValue = calculator.dataset.firstValue;
+            const operator = calculator.dataset.operator;
+            const secondValue = displayedNum;
+            
+            if (firstValue) {
+                if (previousKeyType === 'calculate') {
+                    firstValue = displayedNum;
+                }
+                display.textContent = operate(parseFloat(firstValue), operator, parseFloat(secondValue));
+                
+            }
 
-        function calculate() {
-            array[2] = parseFloat(displayedNum);
-            display.textContent = operate(array[0], array[1], array[2]);
-            console.log(array);
-            array = [display.textContent];
-            operatorIsPressed = false;
+            calculator.dataset.modValue = secondValue;
+            calculator.dataset.previousKeyType = 'calculate';
         }
     }
 });
